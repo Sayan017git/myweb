9c86380cb918effffeda6055e52e1acbacc41442
Sayan017git <83445018+Sayan017git@users.noreply.github.com>
2021-04-30 13:40:12 +0530

    Add files via upload

---
+++ css/js/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-List')
+rightNav = document.querySelector('rightNav')
+
+
+
+burger.addEventListener('class', ()=>{
+         rightNav.classList.toggle('v-class-resp');
+         nav-List.navbar.classList.toggle('v-class-resp');
+         navbar.classList.toggle('h-nav-resp');
+})    
